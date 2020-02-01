100bdcf9d97c3c65da19ceecf841484ed0b4d6b7
cafawo <fabian@woebbeking.info>
2020-02-01 16:39:14 +0100

    Add admin class to form/admin.py

--- form/admin.py
+++ form/admin.py
@@ -2,4 +2,11 @@ from django.contrib import admin
 
 # Register your models here.
 from .models import SomeModel  # <-- NEW
-admin.site.register(SomeModel)  # <-- NEW
+
+# the PostAdmin class is cosmetic (optional)
+class SomeModelAdmin(admin.ModelAdmin):  # <-- NEW
+    list_display = ('date_submitted', 'first_name', 'last_name', 'first_number','second_number')
+    list_filter = ("last_name",)
+    search_fields = ['first_name', 'last_name']
+
+admin.site.register(SomeModel, SomeModelAdmin)  # <-- NEW
