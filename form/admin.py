14b7f1d4d80c2b3278b2799fbdf09190fd66a9ab
cafawo <fabian@woebbeking.info>
2020-04-15 16:26:07 +0200

    Limit results in admin page

--- form/admin.py
+++ form/admin.py
@@ -5,6 +5,7 @@ from .models import SomeModel  # <-- NEW
 
 # the PostAdmin class is cosmetic (optional)
 class SomeModelAdmin(admin.ModelAdmin):  # <-- NEW
+    list_per_page = 20
     list_display = ('date_submitted', 'first_name', 'last_name', 'first_number','second_number')
     list_filter = ("last_name",)
     search_fields = ['first_name', 'last_name']
--- upload/admin.py
+++ upload/admin.py
@@ -5,6 +5,7 @@ from .models import Document  # <-- NEW
 
 # the PostAdmin class is cosmetic (optional)
 class DocumentAdmin(admin.ModelAdmin):  # <-- NEW
+    list_per_page = 20
     list_display = ('timestamp', 'docfile', 'file_name', 'file_type', 'file_size')
     list_filter = ('timestamp',)
     search_fields = ['timestamp', 'docfile']
